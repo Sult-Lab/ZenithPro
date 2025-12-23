8b164dba6ff529cf9d1195946c29ed5698ee080f
techsultann <salamsultan09@gmail.com>
2025-12-23 13:43:20 +0100

    Initial commit

---
+++ app/src/main/java/com/techsultan/zenithpro/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.techsultan.zenithpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.techsultan.zenithpro.ui.theme.ZenithProTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ZenithProTheme {
+
+            }
+        }
+    }
+}
+
